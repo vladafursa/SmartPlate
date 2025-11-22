6a462ab22bbe0a6b2b8ba928d3cdfc3d9bb5a30e
T0337485 <t0337485@my.ntu.ac.uk>
2025-11-22 03:04:05 +0300

    added plate update dto

---
+++ SmartPlate/DTOs/Plate/PlateUpdateDto.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using SmartPlate.Models.Enums;
+
+namespace SmartPlate.DTOs.Plate
+{
+    public class PlateUpdateDto
+    {
+        [Required]
+        public required Guid Id { get; set; }
+
+        // Core identity
+        [StringLength(15, MinimumLength = 1)]
+        public string? RegistrationNumber { get; set; }
+        public PlateType? Type { get; set; }
+        public List<PlateCategory>? Categories { get; set; }
+
+        // Classification
+        [StringLength(50)]
+        public string? Region { get; set; }
+        [Range(1900, 2025)]
+        public int? YearIssued { get; set; }
+
+        // DVLA rules
+        public bool? CanApplyToAnyVehicle { get; set; }
+        public bool? IsAssigned { get; set; }
+        public bool? AvailableAsCertificate { get; set; }
+
+        public PlateSupplyType? Supply { get; set; }
+    }
+}
