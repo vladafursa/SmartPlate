e49873598d442e27053d9eaddab81dbd532bb71c
T0337485 <t0337485@my.ntu.ac.uk>
2025-11-23 00:51:15 +0300

    changed plate ownership response dto to include fields for username and plate number

--- SmartPlate/DTOs/PlateOwnership/PlateOwnershipResponseDTO.cs
+++ SmartPlate/DTOs/PlateOwnership/PlateOwnershipResponseDTO.cs
@@ -6,7 +6,9 @@ namespace SmartPlate.DTOs.PlateOwnership
     {
         public Guid Id { get; set; }
         public Guid PlateId { get; set; }
+        public string PlateRegNumber { get; set; }
         public Guid OwnerId { get; set; }
+        public string OwnerUserName { get; set; }
         public DateTime Start { get; set; }
         public DateTime? End { get; set; }
     }
